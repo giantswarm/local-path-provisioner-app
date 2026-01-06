897d1990ad0e2c1f07ea018e0a528335d0e033de
Simon Weald <glitchcrab-github@simonweald.com>
2026-01-06 11:40:08 +0000

    fix values.yaml patching logic

--- sync/patches/values/patch.sh
+++ sync/patches/values/patch.sh
@@ -13,18 +13,11 @@ cd "${repo_dir}"
 
 echo "Patching values"
 
-# get the upstream sync version from vendir.yml
-UPSTREAM_SYNC_VERSION=$(yq -r .directories[0].contents[0].git.ref ${repo_dir}/vendir.yml)
-
 set -x
 
-# first patch the values file with the generic changes
 git apply "${script_dir_rel}/_values.yaml.patch"
 
 { set +x; } 2>/dev/null
-
-# then patch the chart values with the upstream version
-sed -i -E "s/tag: PLACEHOLDER/tag: ${UPSTREAM_SYNC_VERSION}/" "${CHART_DIR}/values.yaml"
 
 cp "${script_dir_rel}/manifests/values.schema.json" "${CHART_DIR}/values.schema.json"
 
